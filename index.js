7c6588f0c901bea2366c9c730d1557f9900e1b65
Saravanakumar Mylsamy <122189261+SaravanaKumarMylsamy@users.noreply.github.com>
2023-02-01 16:55:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let point1 = 0;
+let point2 = 0;
+function reset(){
+    document.getElementById("sar").textContent = 0
+    document.getElementById("sara").textContent = 0
+}
+function add1(){
+   point1 += 1
+   document.getElementById("sar").textContent = point1
+    
+}
+function add2(){
+    point1 += 2
+    document.getElementById("sar").textContent = point1
+ 
+}
+function add3(){
+    point1 += 3
+    document.getElementById("sar").textContent = point1
+}
+function add4()
+{
+    point2 += 1
+    document.getElementById("sara").textContent = point2
+}
+function add5()
+{
+    point2 += 2
+    document.getElementById("sara").textContent = point2
+}
+function add6()
+{
+    point2 += 3
+    document.getElementById("sara").textContent = point2
+}
